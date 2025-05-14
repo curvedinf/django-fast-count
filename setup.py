91ceaa15bb930c4e5ed5a9ddb2d97ab9e0e4ed23
Chase Adams <chase.adams@gmail.com>
2025-05-14 18:02:13 -0500

    removed tox, added simple publish script

--- setup.py
+++ setup.py
@@ -1,22 +1,36 @@
-"""
-Setup file for django-fast-count.
-Use setup.cfg to configure your project.
+from pathlib import Path
 
-This file was generated with PyScaffold 4.6.
-PyScaffold helps you to put up the scaffold of your new Python project.
-Learn more under: https://pyscaffold.org/
-"""
+from setuptools import find_packages, setup
 
-from setuptools import setup
+# The directory containing this file
+HERE = Path(__file__).parent
 
-if __name__ == "__main__":
-    try:
-        setup(use_scm_version={"version_scheme": "no-guess-dev"})
-    except:  # noqa
-        print(
-            "\n\nAn error occurred while building the project, "
-            "please ensure you have the most updated version of setuptools, "
-            "setuptools_scm and wheel with:\n"
-            "   pip install -U setuptools setuptools_scm wheel\n\n"
-        )
-        raise
+# The text of the README file
+README = (HERE / "README.md").read_text()
+
+setup(
+    name="django-fast-count",
+    version="0.1.0",
+    description="A fast Django .count() implementation for large tables.",
+    long_description=README,
+    long_description_content_type="text/markdown",
+    url="https://github.com/curvedinf/django-fast-count",
+    author="Chase Adams",
+    author_email="chase.adams@gmail.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[
+        "django>=3.2",
+    ],
+    entry_points={
+        "console_scripts": [
+            "dir-assistant=dir_assistant.main:main",
+        ],
+    },
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
