90c8ceb1bb775fe6f15eea77dbf1a129c85f61b0
Chase Adams <chase.adams@gmail.com>
2025-05-14 13:13:51 -0500

    Write an admin for TestModel in djangotest/testapp

--- tests/djangotest/testapp/admin.py
+++ tests/djangotest/testapp/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
+from .models import TestModel
 
-# Register your models here.
+@admin.register(TestModel)
+class TestModelAdmin(admin.ModelAdmin):
+    list_display = ("id", "uuid", "flag")
+    list_filter = ("flag",)
+    search_fields = ("uuid",)
+    readonly_fields = ("uuid",)
